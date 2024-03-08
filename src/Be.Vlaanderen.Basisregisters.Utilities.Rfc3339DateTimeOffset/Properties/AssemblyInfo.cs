ae60ab61d3799726ddb5a7e80c6481f5bb5320fd
jvandaal <jonas.vandaal@outlook.be>
2024-03-08 07:38:41 +0100

    feat: move to dotnet 8.0.2
    move to dotnet 8.0.2
    
    BREAKING CHANGE: move to dotnet 8.0.2

--- src/Be.Vlaanderen.Basisregisters.Utilities.Rfc3339DateTimeOffset/Properties/AssemblyInfo.cs
+++ src/Be.Vlaanderen.Basisregisters.Utilities.Rfc3339DateTimeOffset/Properties/AssemblyInfo.cs
@@ -5,8 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyProduct("Basisregisters Vlaanderen")]
 [assembly: AssemblyDescription("RFC 3339 DateTimeOffset..")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
 [assembly: AssemblyCompany("Vlaamse overheid")]
 
